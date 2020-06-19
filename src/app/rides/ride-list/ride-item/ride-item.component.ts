1162d84bbdecd61c0760f87de0ae3e26a7f146a2
Renee de Vries <r.r.devries@uva.nl>
2020-06-19 12:27:40 +0200

    Added components

---
+++ src/app/rides/ride-list/ride-item/ride-item.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-ride-item',
+  templateUrl: './ride-item.component.html',
+  styleUrls: ['./ride-item.component.css']
+})
+export class RideItemComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
